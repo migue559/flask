0125e8a3702f00f87749aa0614a82d7ef920aac4
Miguel Angel Cruz <miguelangel.cruz@kumma.com.mx>
2025-03-21 21:11:53 -0600

    Create sascfg_psn.py

---
+++ sascfg_psn.py
@@ -0,0 +1,21 @@
+SAS_config_names = ['iomlinux']
+SAS_config_options = {'lock_down': False,
+                      'verbose': True,
+                      'prompt': True
+                      }
+
+
+SAS_output_options = {'output': 'html5'}
+
+
+iomlinux = {
+            'java': '/oracle/client19/19.0.0.0/jdk/bin/java',
+            #'iomhost': 'dbdrcivlmx02.pre.mx.corp',
+            #'iomport': 8592,
+            # 'omruser': 'gcupmsas',
+            #'classpath': cpW,
+            #'omrpw': 'S45j4Npr3.',
+            }
+
+
+
